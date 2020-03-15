9121ff3ae0dbda6c57408ebecf225f923fd9ae4f
Aristóteles Fernandes <fernandessfae@hotmail.com>
2020-03-15 18:03:13 -0300

    produção gás natural (classificação Random Forest)

---
+++ produção gás natural (classificação Random Forest).py
@@ -0,0 +1,44 @@
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix, accuracy_score
+from yellowbrick.classifier import ConfusionMatrix
+
+#Carregamento da base de dados
+dados = pd.read_csv('Anuário Estatístico 2019 - Evolução da produção de gás natural, por localização.csv', sep = ';', decimal = ',')
+
+#Criação de uma variável com variáveis independentes(x)
+previsores = dados.iloc[:, [0, 3]].values
+
+#Criação de uma variável com variável de resposta(y)
+classe = dados.iloc[:, 1].values
+
+#Aqui iremos transformar as colunas categóricas em colunas numéricas
+labelencoder = LabelEncoder()
+previsores[:, 0] = labelencoder.fit_transform(previsores[:, 0])
+
+#Aqui hávera a divisão dos dados para treinamento e teste passando como parâmetros(variavel independente, variável resposta, a amostra de teste[0 até 1] e divisao da base de dados igual)
+X_treinamento, X_teste, y_treinamento, y_teste = train_test_split(previsores, classe, test_size = 0.3, random_state = 0)
+
+#Crição do algoritmo randoom forest usando a base de treinamento
+rf = RandomForestClassifier(n_estimators = 40)
+rf.fit(X_treinamento, y_treinamento)
+
+#Faz a previsão do algoritmo knn usando a base de teste
+previsoes = rf.predict(X_teste)
+
+#Cria uma matriz de confusão nessa variável
+confusao = confusion_matrix(y_teste, previsoes)
+
+#Cria variáveis com a taxa de acerto e erro
+taxa_acerto = accuracy_score(y_teste, previsoes)
+taxa_erro = 1 - taxa_acerto
+
+#Aqui irá gerá a figura da matriz de confusão (executar os 4 comandos simultaneamente)
+v = ConfusionMatrix(rf)
+v.fit(X_treinamento, y_treinamento)
+v.score(X_teste, y_teste)
+v.poof()
+
+''' A taxa de acerto do modelo foi de 93,8%, aproximadamente, com 30% dos dados para teste.'''
