4e7f7f1d0cda6648f1c9cd776529a8add7baced7
Aristóteles Fernandes <fernandessfae@hotmail.com>
2020-03-16 17:49:19 -0300

    produção gás natural (classificação MLP)

---
+++ produção gás natural (classificação MLP).py
@@ -0,0 +1,42 @@
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import confusion_matrix, accuracy_score
+from yellowbrick.classifier import ConfusionMatrix
+
+#Carregamento da base de dados
+dados = pd.read_csv('Anuário Estatístico 2019 - Evolução da produção de gás natural, por localização.csv', sep = ';', decimal = ',')
+
+#Criação de uma variável com variáveis independentes(x)
+previsores = dados.iloc[:, [0, 3]].values
+
+#Criação de uma variável com variável de resposta(y)
+classe = dados.iloc[:, 1].values
+
+#Aqui iremos transformar as colunas categóricas em colunas numéricas
+labelencoder = LabelEncoder()
+previsores[:, 0] = labelencoder.fit_transform(previsores[:, 0])
+
+#Aqui hávera a divisão dos dados para treinamento e teste passando como parâmetros(variavel independente, variável resposta, a amostra de teste[0 até 1] e divisao da base de dados igual)
+X_treinamento, X_teste, y_treinamento, y_teste = train_test_split(previsores, classe, test_size = 0.3, random_state = 0)
+
+#Crição do algoritmo MLPClassifier usando a base de treinamento
+mlp = MLPClassifier(hidden_layer_sizes = (200), max_iter = 3000, tol = 0.0000010, verbose = True)
+mlp.fit(X_treinamento, y_treinamento)
+
+#Faz a previsão do algoritmo MLP usando a base de teste
+previsoes = mlp.predict(X_teste)
+
+#Cria uma matriz de confusão nessa variável
+confusao = confusion_matrix(y_teste, previsoes)
+
+#Cria variáveis com a taxa de acerto e erro
+taxa_acerto = accuracy_score(y_teste, previsoes)
+taxa_erro = 1 - taxa_acerto
+
+#Aqui irá gerá a figura da matriz de confusão (executar os 4 comandos simultaneamente)
+v = ConfusionMatrix(mlp)
+v.fit(X_treinamento, y_treinamento)
+v.score(X_teste, y_teste)
+v.poof()
