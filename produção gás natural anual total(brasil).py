df8fa3df289cf8c180137c74514afcd2f35f882a
Aristóteles Fernandes <fernandessfae@hotmail.com>
2020-05-13 19:40:52 -0300

    Update produção gás natural anual total(brasil).py

--- produção gás natural anual total(brasil).py
+++ produção gás natural anual total(brasil).py
@@ -1,27 +1,22 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-''' Aqui faremos um histograma com a produção total de gás natural (onshore/offshore) no Brasil
-    entre 2009 a 2018.'''
+''' Aqui faremos um histograma com a produção total de gás natural (onshore/offshore) no Brasil entre 2009 a 2018.'''
 
-#Carregamento da base de dados
 dados = pd.read_csv('Anuário Estatístico 2019 - Evolução da produção de gás natural, por localização.csv', sep = ';', decimal = ',')  
 
-#Conversão de milhôes m³ para m³
-dados['Produção de gás natural (milhões m3)'] = dados['Produção de gás natural (milhões m3)'].mul(1000000)
+#Criação da função de dataframe nacional
+def dataframe(x):
+    x['Produção de gás natural (milhões m3)'] = x['Produção de gás natural (milhões m3)'].mul(1000000)
+    x = x.rename(columns = {'Produção de gás natural (milhões m3)' : 'Produção de gás natural (m³)'})
+    x = x.groupby(['Ano'])['Produção de gás natural (m³)'].sum().reset_index()
 
-#Renomeação da coluna
-dados = dados.rename(columns = {'Produção de gás natural (milhões m3)' : 'Produção de gás natural (m³)'})
-
-#Soma de toda a produção de cada ano
-dados = dados.groupby(['Ano'])['Produção de gás natural (m³)'].sum()
-
-dados = dados.reset_index()
+#Alteração do dataframe nacional
+dataframe(dados)
 
 #Fazendo o gráfico para a produção anual total
-plt.figure(figsize = (10, 5))
-plt.bar(dados.iloc[:, 0], dados.iloc[:, 1], color = 'yellow')
-plt.xticks(dados['Ano'])
+plt.figure(figsize = (15, 5))
+plt.bar(dados.iloc[:, 2], dados.iloc[:, 3], color = 'yellow')
 plt.xlabel('Produção Anual')
-plt.ylabel('Total Produção Anual (10^10 m³)')
-plt.title('Produção total de gás natural onshore/offshore em território brasileiro (2009 - 2018)')     
+plt.ylabel('Quantidade (m³)')
+plt.title('Produção total de gás natural onshore/offshore em território brasileiro (2009 - 2018)')
